eefc6d9fdfbbd18802bc54bc5dcfe50693931fea
Nikkinach <nikkinach@gmail.com>
2025-05-09 12:04:19 +0530

    Dealing with discriminator overpowering

--- model/discriminator.py
+++ model/discriminator.py
@@ -2,33 +2,36 @@ import torch
 import torch.nn as nn
 
 class Discriminator(nn.Module):
-    def __init__(self, clip_value=0.01):
+    def __init__(self):
         super().__init__()
-        self.clip_value = clip_value  # Optional clipping
 
-        def block(in_feat, out_feat, stride):
+        def conv_block(in_feat, out_feat, stride):
             return nn.Sequential(
-                nn.utils.spectral_norm(nn.Conv2d(in_feat, out_feat, 3, stride, 1)),
+                nn.Conv2d(in_feat, out_feat, 3, stride, 1),
                 nn.LeakyReLU(0.2, inplace=True)
             )
 
         self.model = nn.Sequential(
-            nn.Conv2d(3, 64, 3, 1, 1),
+            # Spectral Norm ONLY on the first layer
+            nn.utils.spectral_norm(nn.Conv2d(3, 64, 3, 1, 1)),
             nn.LeakyReLU(0.2, inplace=True),
-            block(64, 64, 2),
-            block(64, 128, 1),
-            block(128, 128, 2),
-            block(128, 256, 1),
-            block(256, 256, 2),
-            block(256, 512, 1),
-            block(512, 512, 2),
+
+            conv_block(64, 64, 2),
+            conv_block(64, 128, 1),
+            conv_block(128, 128, 2),
+            conv_block(128, 192, 1),
+            conv_block(192, 192, 2),
+            conv_block(192, 256, 1),
+            conv_block(256, 256, 2),
+
             nn.AdaptiveAvgPool2d(1),
             nn.Flatten(),
-            nn.Linear(512, 1024),
+            nn.Linear(256, 512),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(1024, 1)
+            nn.Dropout(0.3),  # Regularization
+            nn.Linear(512, 1)
         )
 
     def forward(self, x):
-        out = self.model(x)
-        return out
+        return self.model(x)
+
